9defe3dd4577343dd9b646568a76b53aad4b5799
nabeatsu <tanabe.1478@gmail.com>
2020-03-26 20:37:39 +0900

    Enable Raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,17 @@
 use termion;
 
 use std::io::{stdin, stdout, Write};
-use std::sync::mpsc::channel;
-use std::thread;
-use std::time::Duration;
 use termion::event::{Event, Key};
-use termion::input::{MouseTerminal, TermRead};
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::screen::AlternateScreen;
 
 fn main() {
     let stdin = stdin();
 
-    let mut stdout = MouseTerminal::from(AlternateScreen::from(stdout()).into_raw_mode().unwrap());
-    let (tx, rx) = channel();
-
-    thread::spawn(move || {
-        for c in stdin.events() {
-            if let Ok(evt) = c {
-                tx.send(evt).unwrap();
-            }
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    for event in stdin.events() {
+        if event.unwrap() == Event::Key(Key::Ctrl('c')) {
+            return;
         }
-    });
-
-    loop {
-        if let Ok(evt) = rx.recv_timeout(Duration::from_millis(16)) {
-            if evt == Event::Key(Key::Ctrl('c')) {
-                return;
-            }
-        }
-        stdout.flush().unwrap();
     }
 }
